fb8f61626d1fdd427daae59eba5bc76e806bbdeb
Andrew Mason <andrew@fixedpoint.xyz>
2016-01-29 19:24:07 -0500

    implement connecting logic, and start to work on the main loop

---
+++ api.go
@@ -0,0 +1,44 @@
+package slack
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+const (
+	slackURL = "https://slack.com/api/%s"
+)
+
+func (bot *Bot) Call(method string, data url.Values) (map[string]interface{}, error) {
+	data.Set("token", bot.Token)
+	return callAPI(method, data)
+}
+
+func callAPI(method string, data url.Values) (map[string]interface{}, error) {
+	methodURL := fmt.Sprintf(slackURL, method)
+	response, err := http.PostForm(methodURL, data)
+	return httpToJSON(response, err)
+}
+
+func httpToJSON(response *http.Response, err error) (map[string]interface{}, error) {
+	if err != nil {
+		return nil, err
+	}
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+	return unpackJSON(body)
+}
+
+func unpackJSON(body []byte) (map[string]interface{}, error) {
+	var payload map[string]interface{}
+	err := json.Unmarshal(body, &payload)
+	if err != nil {
+		return nil, err
+	}
+	return payload, nil
+}
--- bot.go
+++ bot.go
@@ -1,9 +1,53 @@
 package slack
 
+import (
+	"net/http"
+	"net/url"
+
+	"github.com/gorilla/websocket"
+)
+
 type Bot struct {
 	Token string
 }
 
 func NewBot(token string) *Bot {
 	return &Bot{Token: token}
+}
+
+func (bot *Bot) Start() error {
+	payload, err := bot.Call("rtm.start", url.Values{})
+	if err != nil {
+		return err
+	}
+	success, ok := payload["ok"].(bool)
+	if !(ok && success) {
+		return &SlackError{"could not connect to RTM API"}
+	}
+	websocketURL, _ := payload["url"].(string)
+	return bot.connect(websocketURL)
+}
+
+func (bot *Bot) connect(websocketURL string) error {
+	dialer := websocket.Dialer{}
+	conn, _, err := dialer.Dial(websocketURL, http.Header{})
+	if err != nil {
+		return err
+	}
+	bot.loop(conn)
+	return nil
+}
+
+func (bot *Bot) loop(conn *websocket.Conn) {
+	for {
+		messageType, bytes, err := conn.ReadMessage()
+		if err != nil {
+			// ReadMessage returns an error if the connection is closed
+			conn.Close()
+			return
+		}
+		if messageType == websocket.BinaryMessage {
+			continue // ignore binary messages
+		}
+	}
 }
