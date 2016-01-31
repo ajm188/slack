6d89fcc1ef49c8a5250114d6703eeb04cf6ac9b8
Andrew Mason <andrew@fixedpoint.xyz>
2016-01-30 21:34:06 -0500

    OpenDirectMessage should return a string, not the payload

--- direct_message.go
+++ direct_message.go
@@ -2,8 +2,24 @@ package slack
 
 import (
 	"net/url"
+
+	log "github.com/Sirupsen/logrus"
 )
 
-func (bot *Bot) OpenDirectMessage(userID string) (map[string]interface{}, error) {
-	return bot.Call("im.open", url.Values{"user": []string{userID}})
+func (bot *Bot) OpenDirectMessage(userID string) (string, error) {
+	payload, err := bot.Call("im.open", url.Values{"user": []string{userID}})
+	if err != nil {
+		return "", err
+	}
+	success := payload["ok"].(bool)
+	if !success {
+		log.WithFields(log.Fields{
+			"payload": payload,
+			"userID":  userID,
+			"nick":    bot.Users[userID],
+		}).Error("Failed to open direct message.")
+		return "", &SlackError{"could not open direct message"}
+	}
+	channel := payload["channel"].(map[string]interface{})
+	return channel["id"].(string), nil
 }
