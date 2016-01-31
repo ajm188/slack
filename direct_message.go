0e9d344c1fac579633f7c25d1cdc5387d47ff0df
Andrew Mason <andrew@fixedpoint.xyz>
2016-01-30 21:40:10 -0500

    open a DM and construct a message to it in one go

--- direct_message.go
+++ direct_message.go
@@ -6,6 +6,14 @@ import (
 	log "github.com/Sirupsen/logrus"
 )
 
+func (bot *Bot) DirectMessage(userID, text string) (*Message, Status) {
+	dm, err := bot.OpenDirectMessage(userID)
+	if err != nil {
+		return nil, CONTINUE
+	}
+	return NewMessage(text, dm), CONTINUE
+}
+
 func (bot *Bot) OpenDirectMessage(userID string) (string, error) {
 	payload, err := bot.Call("im.open", url.Values{"user": []string{userID}})
 	if err != nil {
