c876f9ec542cdca3185d5d380c1d7ea95edb1901
Andrew Mason <andrew@fixedpoint.xyz>
2016-02-17 19:28:04 -0500

    stupid test

--- bot_test.go
+++ bot_test.go
@@ -1,8 +1,6 @@
-package slack_test
+package slack
 
 import (
-	. "."
-
 	"testing"
 )
 
@@ -10,5 +8,13 @@ func TestABotHasAToken(t *testing.T) {
 	bot := NewBot("token")
 	if bot.Token != "token" {
 		t.Errorf("Failure. Expected %s; Got %s", "token", bot.Token)
+	}
+}
+
+func TestPrivate_connect(t *testing.T) {
+	bot := NewBot("token")
+	err := bot.connect("junk")
+	if err == nil {
+		t.Error("Error. Expecting error. Got nil")
 	}
 }
