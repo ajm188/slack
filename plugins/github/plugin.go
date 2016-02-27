b5417a4a104108e4e053e8a2c8a1e3baf0a9b06d
Andrew Mason <andrew@fixedpoint.xyz>
2016-02-26 22:45:48 -0500

    add docs

--- plugins/github/plugin.go
+++ plugins/github/plugin.go
@@ -15,12 +15,28 @@ var (
 	}
 )
 
+// The Plugin interface is used to define the interface for all GitHub
+// plugins. Implementing this interface will allow the plugin to use the common
+// `CanLoad` logic.
 type Plugin interface {
 	slack.Plugin
+	// extraEnvVars returns any a list of any additional environment variables
+	// this plugin needs.
 	extraEnvVars() []string
+	// setEnvVar should store the value of an environment variable in this
+	// plugin.
 	setEnvVar(string, string)
 }
 
+// CanLoad can be used to load any generic GitHub plugin. It will ensure that
+// the basic `requiredEnvVars` (GH_CLIENT_ID, GH_CLIENT_SECRET, and
+// GH_ACCESS_TOKEN) are set, as well as any extra environment variables the
+// plugin may depend on. All of these variables' values will be handed to the
+// plugin so it can store them.
+//
+// If all necessary environment variables are set, then CanLoad will return
+// true. Otherwise, it will log at the Error level for each offending
+// environment variable and return false.
 func CanLoad(plugin Plugin) (ok bool) {
 	ok = true
 	for _, evar := range append(requiredEnvVars, plugin.extraEnvVars()...) {
